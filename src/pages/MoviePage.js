f52d5641cb3f64b275cd3ed7214d182ce64c2191
Do Son <sonnvc2001@gmail.com>
2023-02-14 17:16:35 +0700

    212-Fetching dữ liệu trang danh sách phim

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React from "react";
+import MovieList from "../components/movie/MovieList";
+import useSWR from "swr";
+import { fetcher } from "../config";
+import MovieCard from "../components/movie/MovieCard";
 
 const MoviePage = () => {
-    return (
-        <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, eum ipsam magni placeat voluptatibus reiciendis itaque reprehenderit facilis tempora quod, sapiente ea voluptatum ut blanditiis cum beatae laborum tenetur nam.
+  const { data } = useSWR(
+    `https://api.themoviedb.org/3/movie/popular?api_key=9b3a625c85535b4f9746c7e67e91eff1`,
+    fetcher
+  );
+  const movies = data?.results || [];
+
+  return (
+    <div className="py-10 page-container">
+      <div className="flex mb-10">
+        <div className="flex-1">
+          <input
+            type="text"
+            className="w-full p-4 bg-slate-800 text-white outline-none"
+            placeholder="Type here to search..."
+          />
         </div>
-    );
+        <button className="p-4 bg-primary text-white">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="grid grid-cols-4 gap-10">
+        {movies.length > 0 &&
+          movies.map((item) => (
+            <MovieCard key={item.id} item={item}></MovieCard>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default MoviePage;
