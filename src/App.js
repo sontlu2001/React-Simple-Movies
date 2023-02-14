b6a6ad311e7606947c97e06eb7dda31161f4fe6c
Do Son <sonnvc2001@gmail.com>
2023-02-14 16:39:52 +0700

    211-Thiết lập Routes cho dự án

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import { Fragment } from "react";
-import Banner from "./components/banner/Banner";
-import MovieCard from "./components/movie/MovieCard";
-import MovieList from "./components/movie/MovieList";
 import "swiper/scss";
+import { Route, Routes } from "react-router-dom";
+import Main from "./components/layout/Main";
+import HomePage from "./pages/HomePage";
+import Banner from "./components/banner/Banner";
+import MoviePage from "./pages/MoviePage";
 
 function App() {
   return (
     <Fragment>
-      <header className="header mb-5 py-10 flex items-center justify-center gap-x-5 text-white">
-        <span className="text-primary">Home</span>
-        <span>Movie</span>
-      </header>
-
-      <Banner></Banner>
-
-      <section className="movies-layout pb-20 page-container text-white">
-        <h2 className="capitalize mb-10 text-3xl font-bold">Now playing</h2>
-        <MovieList></MovieList>
-      </section>
-
-      <section className="movies-layout pb-20 page-container text-white">
-        <h2 className="capitalize mb-10 text-3xl font-bold">Top rated</h2>
-          <MovieList type="top_rated"></MovieList>
-      </section>
-
-      <section className="movies-layout pb-20 page-container text-white">
-        <h2 className="capitalize mb-10 text-3xl font-bold">Trending</h2>
-          <MovieList type="popular"></MovieList>
-      </section>
-    </Fragment>
+    <Routes>
+      <Route element={<Main></Main>}>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner></Banner>
+              <HomePage></HomePage>
+            </>
+          }
+        ></Route>
+        <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
+      </Route>
+    </Routes>
+  </Fragment>
   );
 }
 
---
+++ src/pages/MoviePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MoviePage = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, eum ipsam magni placeat voluptatibus reiciendis itaque reprehenderit facilis tempora quod, sapiente ea voluptatum ut blanditiis cum beatae laborum tenetur nam.
+        </div>
+    );
+};
+
+export default MoviePage;
