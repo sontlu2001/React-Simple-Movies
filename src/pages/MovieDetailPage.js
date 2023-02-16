b10766280ffb8da83838555d372ca7e3885b3e75
Do Son <sonnvc2001@gmail.com>
2023-02-16 10:29:38 +0700

    215-Trang chi tiết phim Fetching dữ liệu diễn viên

--- src/pages/MovieDetailPage.js
+++ src/pages/MovieDetailPage.js
@@ -6,11 +6,11 @@ import { apiKey, fetcher } from "../config";
 const MovieDetailPage = () => {
   const { movieId } = useParams();
   const { data, error } = useSWR(
-    `https://api.themoviedb.org/3/movie/${movieId}?api_key=9b3a625c85535b4f9746c7e67e91eff1`,
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`,
     fetcher
   );
   if (!data) return null;
-  const { backdrop_path, poster_path, title, genres,overview } = data;
+  const { backdrop_path, poster_path, title, genres, overview } = data;
 
   return (
     <div className="pb-10">
@@ -30,7 +30,7 @@ const MovieDetailPage = () => {
           alt=""
         />
       </div>
-      <h1 className="text-center font-bold text-3xl text-white mb-10">
+      <h1 className="text-center font-bold text-4xl text-white mb-10">
         {title}
       </h1>
       {genres.length > 0 && (
@@ -45,9 +45,59 @@ const MovieDetailPage = () => {
           ))}
         </div>
       )}
-    <p className="text-center leading-relaxed max-w-[700px] mx-auto">{overview}</p>
+      <p className="text-center leading-relaxed max-w-[700px] mx-auto mb-10">
+        {overview}
+      </p>
+      <MovieCredits></MovieCredits>
+      <MoiveVideos></MoiveVideos>
     </div>
   );
 };
 
+// list casts
+function MovieCredits() {
+  const { movieId } = useParams();
+  const { data, error } = useSWR(
+    `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`,
+    fetcher
+  );
+
+  if (!data) return null;
+  const { cast } = data;
+
+  return (
+    <div className="p-10">
+      <h2 className="text-center text-3xl mb-10">Casts</h2>
+      <div className="grid grid-cols-4 gap-5">
+        {cast &&
+          cast.length > 0 &&
+          cast.slice(0,4).map((item) => (
+            <div className="cast-item" key={item.id}>
+              <img
+                className="w-full h-[350px] rounded object-cover mb-3"
+                src={`https://image.tmdb.org/t/p/original/${item.profile_path}`}
+                alt=""
+              />
+              <h3 className="text-xl font-medium">{item.name}</h3>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+function MoiveVideos(){
+  const { movieId } = useParams();
+  const { data, error } = useSWR(
+    `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}`,
+    fetcher
+  );
+
+  if (!data) return null;
+  const { cast } = data;
+
+  return (
+    <div className=""></div>
+  )
+}
 export default MovieDetailPage;
