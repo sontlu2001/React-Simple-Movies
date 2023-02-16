06df6ba5f0896288e2e040d35b7e84b0de52956d
Do Son <sonnvc2001@gmail.com>
2023-02-16 11:27:08 +0700

    216-Trang chi tiết phim Fetching dữ liệu trailers và similar

--- src/pages/MovieDetailPage.js
+++ src/pages/MovieDetailPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { SwiperSlide,Swiper } from "swiper/react";
 import useSWR from "swr";
+import MovieCard from "../components/movie/MovieCard";
 import { apiKey, fetcher } from "../config";
 
 const MovieDetailPage = () => {
@@ -37,7 +39,7 @@ const MovieDetailPage = () => {
         <div className="flex items-center justify-center gap-x-5 mb-10">
           {genres.map((item) => (
             <span
-              className="cursor-pointer py-2 px-4 border border-primary text-primary rounded-md"
+              className="py-2 px-4 border border-primary text-primary rounded-md"
               key={item.id}
             >
               {item.name}
@@ -50,6 +52,7 @@ const MovieDetailPage = () => {
       </p>
       <MovieCredits></MovieCredits>
       <MoiveVideos></MoiveVideos>
+      <MovieSimilar></MovieSimilar>
     </div>
   );
 };
@@ -61,17 +64,16 @@ function MovieCredits() {
     `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`,
     fetcher
   );
-
   if (!data) return null;
   const { cast } = data;
 
   return (
     <div className="p-10">
-      <h2 className="text-center text-3xl mb-10">Casts</h2>
+      <h2 className="text-center font-medium text-3xl mb-10">Casts</h2>
       <div className="grid grid-cols-4 gap-5">
         {cast &&
           cast.length > 0 &&
-          cast.slice(0,4).map((item) => (
+          cast.slice(0, 4).map((item) => (
             <div className="cast-item" key={item.id}>
               <img
                 className="w-full h-[350px] rounded object-cover mb-3"
@@ -86,18 +88,72 @@ function MovieCredits() {
   );
 }
 
-function MoiveVideos(){
+function MoiveVideos() {
   const { movieId } = useParams();
   const { data, error } = useSWR(
     `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}`,
     fetcher
   );
-
   if (!data) return null;
-  const { cast } = data;
+  const { results } = data;
+  if (!results || results.length <= 0) return null;
 
   return (
-    <div className=""></div>
-  )
+    <div className="py-10">
+      <div className="flex flex-col gap-10">
+        {results.slice(0, 1).map((item) => (
+          <div className="text-center" key={item.id}>
+            <h2 className="text-center font-medium text-3xl mb-10">Trailer</h2>
+            <div
+              className="w-full max-w-[1280px] mx-auto aspect-video"
+              key={item.id}
+            >
+              <iframe
+                width="1464"
+                height="563"
+                src={`https://www.youtube.com/embed/${item.key}`}
+                title={item.name}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+                className="w-full h-full object-fill"
+              ></iframe>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function MovieSimilar() {
+  const { movieId } = useParams();
+  const { data, error } = useSWR(
+    `https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=${apiKey}`,
+    fetcher
+  );
+  if (!data) return null;
+  const { results } = data;
+  if (!results || results.length <= 0) return null;
+  console.log(
+    "🚀 ~ file: MovieDetailPage.js:139 ~ MovieSimilar ~ results",
+    results
+  );
+
+  return (
+    <div className="py-10">
+      <h2 className="text-center font-medium text-3xl mb-10">Movie similar</h2>
+      <div className="movie-list">
+        <Swiper grabCursor={"true"} spaceBetween={40} slidesPerView={"auto"}>
+          {results.length > 0 &&
+            results.map((item) => (
+              <SwiperSlide key={item.id}>
+                <MovieCard item={item}></MovieCard>
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 }
 export default MovieDetailPage;
