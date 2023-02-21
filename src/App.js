455f1d6acfe44dd4459b31caed6f2e6d8c0d6d11
Do Son <sonnvc2001@gmail.com>
2023-02-21 09:45:15 +0700

    Code splitting Routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import { Fragment } from "react";
+import { Fragment, lazy, Suspense } from "react";
 import "swiper/scss";
 import { Route, Routes } from "react-router-dom";
 import Main from "./components/layout/Main";
-import HomePage from "./pages/HomePage";
 import Banner from "./components/banner/Banner";
-import MoviePage from "./pages/MoviePage";
-import MovieDetailPage from "./pages/MovieDetailPage";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const MoviePage = lazy(() => import("./pages/MoviePage"));
+const MovieDetailPage = lazy(() => import("./pages/MovieDetailPage"));
 
 function App() {
   return (
     <Fragment>
-    <Routes>
-      <Route element={<Main></Main>}>
-        <Route
-          path="/"
-          element={
-            <>
-              <Banner></Banner>
-              <HomePage></HomePage>
-            </>
-          }
-        ></Route>
-        <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
-        <Route path="/movie/:movieId" element={<MovieDetailPage></MovieDetailPage>}></Route>
-      </Route>
-    </Routes>
-  </Fragment>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route element={<Main></Main>}>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Banner></Banner>
+                  <HomePage></HomePage>
+                </>
+              }
+            ></Route>
+            <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
+            <Route
+              path="/movie/:movieId"
+              element={<MovieDetailPage></MovieDetailPage>}
+            ></Route>
+          </Route>
+        </Routes>
+      </Suspense>
+    </Fragment>
   );
 }
 
